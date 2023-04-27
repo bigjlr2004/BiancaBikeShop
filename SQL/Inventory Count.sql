7f4bd0f819d4b2700638fac70f24f460fa8bbdf2
John Roberts <bigjlr2004@yahoo.com>
2023-04-27 09:33:37 -0500

    Biancas Bike Shope Complete

---
+++ SQL/Inventory Count.sql
@@ -0,0 +1,33 @@
+﻿ SELECT bk.Id, bk.Brand, bk.Color,
+                            own.Id as OwnerId, own.Name, own.Address, own.Email, own.Telephone,
+                            bt.Name,
+                            wo.Id as WorkOrderId, wo.Description, wo.DateInitiated, wo.DateCompleted
+                            FROM Bike bk 
+                                JOIN Owner own on bk.OwnerId = own.Id
+                                JOIN BikeType bt on bk.BikeTypeId = bt.Id
+                                JOIN WorkOrder wo on wo.BikeId = bk.Id
+
+                                 SELECT bk.Id as BikeId, bk.Brand, bk.Color,
+                            own.Id as OwnerId, own.Name, own.Address, own.Email, own.Telephone,
+                            bt.Id as btId, bt.Name,
+                            wo.Id as WorkOrderId, wo.Description, wo.DateInitiated, wo.DateCompleted
+                            FROM Bike bk 
+                                JOIN Owner own on bk.OwnerId = own.Id
+                                JOIN BikeType bt on bk.BikeTypeId = bt.Id
+                                JOIN WorkOrder wo on wo.BikeId = bk.Id
+                            WHERE wo.DateCompleted is NULL
+                             group by BikeId
+                              
+                              
+
+                                   SELECT count(distinct  bk.Id) as BikeId 
+                            
+                            FROM Bike bk 
+                                JOIN Owner own on bk.OwnerId = own.Id
+                                JOIN BikeType bt on bk.BikeTypeId = bt.Id
+                                JOIN WorkOrder wo on wo.BikeId = bk.Id
+                            WHERE wo.DateCompleted is NULL
+                            group by BikeId
+                                                        order BY BikeId
+
+                             
